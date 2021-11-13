e8a4ac9f21488e6168657a1c600b78cd6dd126ab
Khurshidul <khurshidulalam5@gmail.com>
2021-11-13 16:23:53 +0600

    assignment

--- src/Pages/Login/Login/Firebase/firebase.config.js
+++ src/Pages/Login/Login/Firebase/firebase.config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDt-Ctr9FKkxDjD90Frb_azDi9TBwdDkbI",
-    authDomain: "bikes-zone.firebaseapp.com",
-    projectId: "bikes-zone",
-    storageBucket: "bikes-zone.appspot.com",
-    messagingSenderId: "62357856530",
-    appId: "1:62357856530:web:0513d8981ddaf6d80353cb"
+    apiKey: "AIzaSyCCdKHHd_SFSq3ETbgGcH3sCTKPWAy6hDo",
+    authDomain: "genius-car-mechanic-6de64.firebaseapp.com",
+    projectId: "genius-car-mechanic-6de64",
+    storageBucket: "genius-car-mechanic-6de64.appspot.com",
+    messagingSenderId: "428751650946",
+    appId: "1:428751650946:web:e215f56714fe5fb31d349b"
 };
 export default firebaseConfig;
